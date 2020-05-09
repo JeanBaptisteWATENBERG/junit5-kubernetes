9ed16b1432d72339732c50d1068416685bd2ef75
hellysamael <c.etancelin@gmail.com>
2020-05-09 22:36:47 +0200

    test api client

--- core/src/test/java/com/github/jeanbaptistewatenberg/TestKube.java
+++ core/src/test/java/com/github/jeanbaptistewatenberg/TestKube.java
@@ -1,6 +1,9 @@
 package com.github.jeanbaptistewatenberg;
 
 import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
 import io.kubernetes.client.util.Config;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -8,40 +11,31 @@ import okhttp3.Response;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 
+import javax.net.ssl.*;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 public class TestKube {
     OkHttpClient okHttpClient = new OkHttpClient();
     private static final Logger LOGGER = Logger.getLogger(TestKube.class.getName());
-    @Test
-    void test_kube() {
 
-        Request request = new Request.Builder()
-                .url("https://127.0.0.1:32768/api/v1/namespaces/default")
-                .build();
-        try {
-            Response response = okHttpClient.newCall(request).execute();
-            int code = response.code();
-            LOGGER.info(String.valueOf(code));
-            System.out.println(code);
-            String body = IOUtils.toString(response.body().byteStream());
-            LOGGER.info(body);
-            System.out.println(body);
-        } catch (IOException e) {
-            LOGGER.severe(e.getMessage());
-        }
-    }
 
     @Test
     void test_api_client() {
         try {
             ApiClient client = Config.defaultClient();
-            String basePath = client.getBasePath();
-            System.out.println(basePath);
-            LOGGER.info(basePath);
-        } catch (IOException e) {
+            // infinite timeout
+            OkHttpClient httpClient = client.getHttpClient().newBuilder().readTimeout(0, TimeUnit.SECONDS).build();
+            client.setHttpClient(httpClient);
+            Configuration.setDefaultApiClient(client);
+
+            CoreV1Api coreV1Api = new CoreV1Api(client);
+            System.out.println(coreV1Api.getAPIResources().getResources());
+        } catch (IOException | ApiException e) {
             LOGGER.severe(e.getMessage());
         }
     }
+
+
 }

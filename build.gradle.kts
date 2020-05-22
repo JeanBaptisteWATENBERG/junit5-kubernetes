93fb01f2944fe16f9042362e9c4ba590193064b2
Jean-Baptiste WATENBERG <jeanbaptiste.watenberg@gmail.com>
2020-05-22 20:30:18 +0200

    chore: bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 subprojects {
     group = "com.github.jeanbaptistewatenberg.junit5kubernetes"
-    version = "1.0.0"
+    version = "2.0.0"
 
     repositories {
         mavenCentral()
